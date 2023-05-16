fb9702e256449e53f330455ea08984ea1a081783
“Abhinav <vabhinav368@gmail.com>
2023-05-16 22:12:34 +0530

    added register functionality

--- app/(site)/components/authform/index.tsx
+++ app/(site)/components/authform/index.tsx
@@ -6,6 +6,7 @@ import React, { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "../authSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
 
 type FormVariant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
@@ -36,6 +37,7 @@ const AuthForm = () => {
       // NEXTAUTH SIGNIN
     } else if (formVariant === "REGISTER") {
       //REGISTER USER
+      axios.post("/api/register", data);
     }
   };
 
