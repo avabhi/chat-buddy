b14060a9eb7f7f6675dd188711f88f266e1b35fd
“Abhinav <vabhinav368@gmail.com>
2023-05-18 01:04:15 +0530

    setting up the routes

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,10 @@
 import { withAuth } from "next-auth/middleware";
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 export default withAuth({
   pages: {
     signIn: "/",
   },
 });
-
-// export default withAuth(
-//   // `withAuth` augments your `Request` with the user's token.
-//   function middleware(req) {
-//     console.log(req.nextauth.token);
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token }) => token?.role === "admin",
-//     },
-//     pages: {
-//       signIn: "/",
-//     },
-//   }
-// );
-
-// export const config = { matcher: ["/admin"] };
-// export default withAuth({
-//   callbacks: {
-//     authorized: async ({ req, token }) => {
-//       const pathname = req.nextUrl.pathname;
-
-//       if (pathname.startsWith("/_next") || pathname === "/favicon.ico")
-//         return true;
-
-//       if (token) return true;
-
-//       return false;
-//     },
-//   },
-//   pages: {
-//     signIn: "/",
-//   },
-// });
-// export function middleware(request: NextRequest) {
-//   console.log("lol!!!");
-// }
 
 export const config = {
   matcher: ["/conversations/:path*", "/users/:path*"],
