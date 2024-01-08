719429e2544896676d409ac7cc35c29238cfe813
Abhinav <vabhinav368@gmail.com>
2024-01-08 23:52:13 +0530

    small css changes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,14 +6,14 @@ export default function Home() {
     <div className="flex min-h-full flex-col jusitfy-center py-12 sm:px-6 lg:px-8 bg-gray-100 justify-center">
       {/* removing sm:mx-auto because it breaking the center of ui */}
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image
+        {/* <Image
           alt="Logo"
           src="/images/logo.png"
           width="48"
           height="48"
           className="mx-auto w-auto"
-        />
-        <h2 className="mt-6 text-center text-3xl  font-bold tracking-tight text-gray-900">
+        /> */}
+        <h2 className="mt-6 text-center text-3xl  font-medium tracking-tight text-gray-900">
           Sign in to Your Account
         </h2>
       </div>
